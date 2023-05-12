b3e267c9d503b4ab26595b4b6ca8e948ced89865
ayankarmakar12 <ayan.iiuc@gmail.com>
2023-05-12 21:14:58 +0600

    12th commit

--- frontend/component/Navbar.jsx
+++ frontend/component/Navbar.jsx
@@ -16,3 +16,29 @@ const Navbar = () => {
           <div className={styles.text}>012 345 678</div>
         </div>
       </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <Link href="/" passHref>
+            <li className={styles.listItem}>Homepage</li>
+          </Link>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="" width="160px" height="69px" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <Link href="/cart" passHref>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" alt="" width="30px" height="30px" />
+            <div className={styles.counter}>{quantity}</div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
