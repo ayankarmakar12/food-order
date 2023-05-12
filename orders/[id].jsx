578e7d6a89b3fc3caa2876e986faadbe0912d177
ayankarmakar12 <ayan.iiuc@gmail.com>
2023-05-12 23:05:56 +0600

    23rd commit

--- orders/[id].jsx
+++ orders/[id].jsx
@@ -21,4 +21,34 @@ const Order = ({ order }) => {
               <th>Address</th>
               <th>Total</th>
             </tr>
-            <tr className={styles.tr}></tr>
+            <tr className={styles.tr}>
+            </tr>
+            <td>
+                <span className={styles.id}>{order._id}</span>
+              </td>
+              <td>
+                <span className={styles.name}>{order.customer}</span>
+              </td>
+              <td>
+                <span className={styles.address}>{order.address}</span>
+              </td>
+              <td>
+                <span className={styles.total}>${order.total}</span>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div className={styles.row}>
+          <div className={statusClass(0)}>
+            <Image src="/img/paid.png" width={30} height={30} alt="" />
+            <span>Payment</span>
+            <div className={styles.checkedIcon}>
+              <Image
+                className={styles.checkedIcon}
+                src="/img/checked.png"
+                width={20}
+                height={20}
+                alt=""
+              />
+            </div>
+          </div>
