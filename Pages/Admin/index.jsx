7f35d8880b98db2c46b258e96413496986fba409
ayankarmakar12 <ayan.iiuc@gmail.com>
2023-05-12 22:39:11 +0600

    18th commit

---
+++ Pages/Admin/index.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../../styles/Admin.module.css";
+
+const Index = ({ orders, products }) => {
+  const [pizzaList, setPizzaList] = useState(products);
+  const [orderList, setOrderList] = useState(orders);
+  const status = ["preparing", "on the way", "delivered"];
+
+  const handleDelete = async (id) => {
+    console.log(id);
+    try {
+      const res = await axios.delete(
+        "http://localhost:3000/api/products/" + id
+      );
+      setPizzaList(pizzaList.filter((pizza) => pizza._id !== id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
