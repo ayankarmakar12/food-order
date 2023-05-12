c6ded680014c57e2ab344382469fce2b8f08ba63
ayankarmakar12 <ayan.iiuc@gmail.com>
2023-05-12 23:12:53 +0600

    26th commit

---
+++ product/[id].jsx
@@ -0,0 +1,30 @@
+import styles from "../../styles/Product.module.css";
+import Image from "next/image";
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../../redux/cartSlice";
+
+const Product = ({ pizza }) => {
+  const [price, setPrice] = useState(pizza.prices[0]);
+  const [size, setSize] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+  const [extras, setExtras] = useState([]);
+  const dispatch = useDispatch();
+
+  const changePrice = (number) => {
+    setPrice(price + number);
+  };
+
+  const handleSize = (sizeIndex) => {
+    const difference = pizza.prices[sizeIndex] - pizza.prices[size];
+    setSize(sizeIndex);
+    changePrice(difference);
+  };
+
+  const handleChange = (e, option) => {
+    const checked = e.target.checked;
+
+    if (checked) {
+      changePrice(option.price);
+      setExtras((prev) => [...prev, option]);
