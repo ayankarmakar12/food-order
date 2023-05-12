57b50d1f2b184b843efaec9d9fedb8408774eb28
ayankarmakar12 <ayan.iiuc@gmail.com>
2023-05-12 17:54:43 +0600

    1st commit

---
+++ frontend/component/Add.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import styles from "../styles/Add.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const Add = ({ setClose }) => {
+  const [file, setFile] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [prices, setPrices] = useState([]);
+  const [extraOptions, setExtraOptions] = useState([]);
+  const [extra, setExtra] = useState(null);
+
+  const changePrice = (e, index) => {
+    const currentPrices = prices;
+    currentPrices[index] = e.target.value;
+    setPrices(currentPrices);
+  };
