ddb470c1facd9199ebc317d528e943cc13408961
ayankarmakar12 <ayan.iiuc@gmail.com>
2023-05-12 23:13:50 +0600

    27th commit

--- product/[id].jsx
+++ product/[id].jsx
@@ -28,3 +28,39 @@ const Product = ({ pizza }) => {
     if (checked) {
       changePrice(option.price);
       setExtras((prev) => [...prev, option]);
+    } else {
+        changePrice(-option.price);
+        setExtras(extras.filter((extra) => extra._id !== option._id));
+      }
+    };
+  
+    const handleClick = () => {
+      dispatch(addProduct({...pizza, extras, price, quantity}));
+    };
+  
+    return (
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <div className={styles.imgContainer}>
+            <Image src={pizza.img} objectFit="contain" layout="fill" alt="" />
+          </div>
+        </div>
+        <div className={styles.right}>
+          <h1 className={styles.title}>{pizza.title}</h1>
+          <span className={styles.price}>${price}</span>
+          <p className={styles.desc}>{pizza.desc}</p>
+          <h3 className={styles.choose}>Choose the size</h3>
+          <div className={styles.sizes}>
+            <div className={styles.size} onClick={() => handleSize(0)}>
+              <Image src="/img/size.png" layout="fill" alt="" />
+              <span className={styles.number}>Small</span>
+            </div>
+            <div className={styles.size} onClick={() => handleSize(1)}>
+              <Image src="/img/size.png" layout="fill" alt="" />
+              <span className={styles.number}>Medium</span>
+            </div>
+            <div className={styles.size} onClick={() => handleSize(2)}>
+              <Image src="/img/size.png" layout="fill" alt="" />
+              <span className={styles.number}>Large</span>
+            </div>
+          </div>
