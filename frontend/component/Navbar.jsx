02c9eaa30fbe6b60019215f81d08e8b044c235e0
ayankarmakar12 <ayan.iiuc@gmail.com>
2023-05-12 21:12:17 +0600

    11th commit

---
+++ frontend/component/Navbar.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+import { useSelector } from "react-redux";
+import Link from "next/link";
+
+const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>012 345 678</div>
+        </div>
+      </div>
