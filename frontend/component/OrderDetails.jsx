3c8bc5254582435cdf8d481f14c19dec7b805a1e
ayankarmakar12 <ayan.iiuc@gmail.com>
2023-05-12 21:22:45 +0600

    13th commit

---
+++ frontend/component/OrderDetails.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import styles from "../styles/OrderDetail.module.css";
+
+const OrderDetail = ({ total, createOrder }) => {
+  const [customer, setCustomer] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleClick = () => {
+    createOrder({ customer, address, total, method: 0 });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>You will pay $12 after delivery.</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Name Surname</label>
+          <input
+            placeholder="John Doe"
+            type="text"
+            className={styles.input}
+            onChange={(e) => setCustomer(e.target.value)}
+          />
+        </div>
