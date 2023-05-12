3d6208944ec2379ecbff658a220c5a7ccf4ebe1e
ayankarmakar12 <ayan.iiuc@gmail.com>
2023-05-12 22:47:18 +0600

    20th commit

---
+++ Pages/Admin/login.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import styles from "../../styles/Login.module.css";
+
+const Login = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [error, setError] = useState(false);
+  const router = useRouter();
+
+  const handleClick = async () => {
+    try {
+      await axios.post("http://localhost:3000/api/login", {
+        username,
+        password,
+      });
+      router.push("/admin");
+    } catch (err) {
+      setError(true);
+    }
+  };
